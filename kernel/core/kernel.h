3995303b8a6f3f0fb0337ed57215a59c58103b3a
kazah-png <kazah.png@proton.me>
2026-09-05 21:01:48 +0200

    v6.5.128 - base32: RFC 4648 encode/decode (xbm port)

--- kernel/core/kernel.h
+++ kernel/core/kernel.h
@@ -18,7 +18,7 @@
 // Constantes
 // ============================================================
 #define KERNEL_NAME    "NyxOS"
-#define KERNEL_VERSION "6.5.127"
+#define KERNEL_VERSION "6.5.128"
 #define KERNEL_CODENAME "GUI Suite"
 #define KERNEL_DATE    "2026"
 
